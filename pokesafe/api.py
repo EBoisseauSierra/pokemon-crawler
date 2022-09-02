edcbc14a53f0aa38735ff9af5eb30b868781e8ed
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2022-09-02 13:35:28 +0100

    refactor: Extract listing of Pokemons to extract into own method
    Signed-off-by: Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>

--- pokesafe/api.py
+++ pokesafe/api.py
@@ -3,11 +3,12 @@
 import requests
 
 API_BASE_URL = "https://pokeapi.co/api/v2"
+POKEMON_DETAILS_API = "pokemon"
 
 
-def get_pokemon_details(url_suffix: str) -> dict:
-    """Return details of a Pokemon based on a API pointer."""
-    url_to_query = f"{API_BASE_URL}/{url_suffix}"
+def get_pokemon_details(pokemon_id: str) -> dict:
+    """Return details of a Pokemon based on a Pokemon ID."""
+    url_to_query = f"{API_BASE_URL}/{POKEMON_DETAILS_API}/{pokemon_id}"
     api_response = requests.get(url_to_query).json()
 
     name = api_response.get("name")
--- pokesafe/pokemons.py
+++ pokesafe/pokemons.py
@@ -5,11 +5,16 @@ from pokesafe.models import Pokemon
 
 def catch_pokemons() -> None:
     """Catch Pokemons from API and store them in database."""
-    for pokemon_to_catch in ["pokemon/3", "pokemon/2"]:
-        pokemon_details = get_pokemon_details(pokemon_to_catch)
+    for pokemon_id in _get_list_of_pokemon_ids():
+        pokemon_details = get_pokemon_details(pokemon_id)
         Pokemon.objects.update_or_create(**pokemon_details)
 
 
 def clear_pokemons() -> None:
     """Clear previously caught Pokemons."""
     Pokemon.objects.all().delete()
+
+
+def _get_list_of_pokemon_ids() -> list[str]:
+    """Retrieve the list of IDs for the Pokemons to catch."""
+    return [2, 3]
