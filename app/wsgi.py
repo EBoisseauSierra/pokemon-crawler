0a2909df63ddd543a2d137583f1898abe0b8dab3
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2022-09-02 12:56:44 +0100

    refactor: Move Pokemon catching in own method
    Signed-off-by: Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>

--- app/wsgi.py
+++ app/wsgi.py
@@ -12,16 +12,13 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-from pokesafe.api import get_pokemon_details
-from pokesafe.models import Pokemon
+from pokesafe.catch_pokemon import catch_pokemon
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 port = int(os.environ.get("PORT", 8000))
 
 
 # initialize db by load a couple Pokemons
-for pokemon_to_catch in ["pokemon/3", "pokemon/2"]:
-    pokemon_details = get_pokemon_details(pokemon_to_catch)
-    Pokemon.objects.update_or_create(**pokemon_details)
+catch_pokemon()
 
 application = Cling(get_wsgi_application())
---
+++ pokesafe/catch_pokemon.py
@@ -0,0 +1,10 @@
+"""Detail how to retrieve Pokemons."""
+from pokesafe.api import get_pokemon_details
+from pokesafe.models import Pokemon
+
+
+def catch_pokemon() -> None:
+    """Catch Pokemons from API and store them in database."""
+    for pokemon_to_catch in ["pokemon/3", "pokemon/2"]:
+        pokemon_details = get_pokemon_details(pokemon_to_catch)
+        Pokemon.objects.update_or_create(**pokemon_details)
